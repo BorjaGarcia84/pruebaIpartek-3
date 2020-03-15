fe4d7af13e4b9e1c8a07ecbfe63df60ce81abdd1
BorjaGarcia84 <borjagarcia1984@gmail.com>
2020-03-15 22:51:27 +0100

    CasiListo

---
+++ base.sql
@@ -0,0 +1,5 @@
+
+SELECT apellidoE
+FROM EMPLEADO E, DEPARTAMENTO D
+WHERE E.id_departamento = D.id
+ORDER BY apellido
--- index.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT * e.apellido Apellido,d.nombre NombreDepartamento ;
-FROM empleado.e,
-inner join departamento.d,
-on e.id_departamento = d.id,
-order by 1 asc
-
